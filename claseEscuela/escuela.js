4c460b430a159c614846c03fc48d2fd74fd8c695
aimesananton <aimesananton@gmail.com>
2026-06-16 10:06:53 -0300

    codigo terminado

--- claseEscuela/escuela.js
+++ claseEscuela/escuela.js
@@ -4,22 +4,23 @@ class Escuela{
         this.aniofundacion=aniofundacion;
     }
 
-    agregarEstudiante(matricula, promedio){
-        const nuevoEstudiante = new Estudiante()
+    agregarEstudiante(nombre, direccion, contacto, matricula, promedio){
+        const nuevoEstudiante = new Estudiante(nombre, direccion, contacto, matricula, promedio)
+        console.log(`se agrego a ${nuevoEstudiante}`)
     }
 
-    agregarProfesor(){
-
-
+    agregarProfesor(nombre, direccion, contacto, salario){
+        const nuevoProfesor = new Profesor(nombre, direccion, contacto, salario)
+        console.log(`se agrego a ${nuevoProfesor}`)
     }
 
-
-
-    agregarAula(){
-
+    agregarAula(capacidad, profesor, grado){
+        const nuevaAula = new Aula(capacidad, profesor, grado)
+        console.log(`se agrego a ${nuevaAula}`)
     }
 
-    agregarPreceptoria(){
-
+    agregarPreceptoria(capacidad, archivero, contacto){
+        const nuevaPreceptoria = new Preceptoria(capacidad, archivero, contacto)
+        console.log(`se agrego a ${nuevoEstudiante}`)
     }
 }
